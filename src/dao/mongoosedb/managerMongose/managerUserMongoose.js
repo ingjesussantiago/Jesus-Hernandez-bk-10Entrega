8471b8816184eb206fbec5f33846e0e93600598a
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2023-10-03 09:16:55 -0600

    addicion de carrito apartir de req.session.user y si tiene vacio o no el carrito

--- src/dao/mongoosedb/managerMongose/managerUserMongoose.js
+++ src/dao/mongoosedb/managerMongose/managerUserMongoose.js
@@ -7,20 +7,30 @@ export default class ManagerCart {
     addCartToUser = async (id) => {
         try {
 
-            const user = await userModel.findById(id)
+            const user = await userModel.findById(id).populate("cart._id")
             console.log("getuser", user);
             if (user.cart.length === 0) {
                 console.log("El carrito está vacío");
 
                 const cart = await cartModel.create({});
-                console.log(cart);   
-                
-                //ahora hay que insertar el carrito al usuario
+                console.log(cart);
 
+
+                const insertCartToUser = await userModel.updateOne({
+                    _id: user._id
+                }, {
+                    $set: { cart: cart }
+                },
+                    (err, result) => {
+                        if (err) throw err;
+                        console.log('Carrito asociado al usuario correctamente');
+                        client.close(); // Cerrar la conexión a la base de datos
+                    }
+                ).exec();
 
             } else {
-                console.log("El carrito contiene elementos");
-            }
+            console.log("El carrito contiene elementos");
+             }
 
             return user
 
