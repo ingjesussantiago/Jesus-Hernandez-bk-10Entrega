30438af2ca0812aaaeac8afa8d2ec39174eb7480
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2023-10-04 11:13:15 -0600

    fin de dia

--- src/dao/mongoosedb/managerMongose/managerUserMongoose.js
+++ src/dao/mongoosedb/managerMongose/managerUserMongoose.js
@@ -1,5 +1,6 @@
 import userModel from "../models/user.model.js"
 import { cartModel } from "../models/cart.model.js";
+import { productoModel } from "../models/producto.model.js"
 import mongoose, { get } from "mongoose";
 
 
@@ -8,7 +9,7 @@ export default class ManagerCart {
     addCartToUser = async (id) => {
         try {
 
-            const user = await userModel.findById(id).populate("cart._id")
+            const user = await userModel.findById(id).populate("cart._id").lean()
             console.log("getuser", user);
             if (user.cart.length === 0) {
                 console.log("El carrito está vacío");
@@ -27,22 +28,22 @@ export default class ManagerCart {
                         console.log('Carrito asociado al usuario correctamente');
                         client.close(); // Cerrar la conexión a la base de datos
                     }
-                ).exec();
+                ).exec().lean()
 
             } else {
                 // console.log("El carrito contiene elementos");
                 const cart = await userModel.findById(user._id)
                 const cartid = new mongoose.Types.ObjectId(cart.cart[0])
                 // console.log("desde ",cartid);
-                const getCartId = await cartModel.findById(cartid)
+                const getCartId = await cartModel.findById(cartid).populate("products._id").lean()
 
                 // console.log("getCartId",getCartId.products);
 
-                return { productos: getCartId.products }
+                return { products: getCartId.products }
 
             }
 
-            return productos
+            return products
 
 
 
@@ -52,6 +53,46 @@ export default class ManagerCart {
         }
 
     }
+
+
+    addproductToCart = async (id) => {
+        try {
+            console.log("iduser", id);
+            const user = await userModel.findById(id).populate("cart._id")
+            console.log("getaddproduct", user)
+            const cart = await userModel.findById(user._id)
+            const cartid = new mongoose.Types.ObjectId(cart.cart[0])
+            console.log("desde ",cartid);
+            return cartid
+          
+
+            // console.log("getCartId",getCartId.products);
+
+            // return { productos: getCartId.products }
+
+    
+        } catch (error) {
+            console.error('Error al traer add carrito:', err.message);
+            return [];
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -2,6 +2,8 @@ import { Router } from "express"
 import { __dirname } from "../../utils.js"
 import managerUser from "../dao/mongoosedb/managerMongose/managerUserMongoose.js"
 
+
+
 const router = Router()
 
 const ManagerUser = new managerUser()
@@ -20,10 +22,32 @@ router.get("/addCart", async (req, res) => {
 
             const products = await ManagerUser.addCartToUser(id)
 
-            res.render("carrito", {products:products.productos })
+            res.render("carrito", {products:products.products })
+
+            //  res.json({products:products.products })
+            console.log(products);
 
 
-            // res.json({products:products.productos })
+        } else {
+            return res.status(403).send('Usuario no se ha logeado o no resgistrado ')
+        }
+    } catch (error) {
+
+    }
+
+
+})
+
+
+router.get("/addproduct", async (req, res) => {
+    try {
+        if (req.session.user && req.session.user.id) {
+            const { id } = req.session.user
+        
+            const products = await ManagerUser.addproductToCart(id)
+
+            // res.render("carrito", {products:products.productos })
+            res.json(products)
 
 
         } else {
@@ -39,3 +63,12 @@ router.get("/addCart", async (req, res) => {
 
 
 export default router
+
+// try {
+//     const producto = await productoModel.findById(_id).lean()
+//     //  console.log("desdemanager",producto);
+//     return producto
+
+// } catch (error) {
+//     console.log(error);
+// }
